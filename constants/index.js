833525441aae6c7652e6c9b610399bc499423186
Funmilayo Gbadebo <miasmith64200@gmail.com>
2026-05-30 22:26:43 +0100

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,255 @@
+ //ARTS
+const pagesInfo = [
+  {
+    id: 1,
+    image: "pfp.jpeg",
+    title: "I eat doge",
+  },
+  {
+    id: 2,
+    image: "allo-pass.jpeg",
+    title: "Allo Pass",
+  },
+  {
+    id: 3,
+    image: "abby.jpeg",
+    title: "Abby 1",
+  },
+  {
+    id: 4,
+    image: "abby2.jpeg",
+    title: "Abby 2",
+  },
+  {
+    id: 5,
+    image: "toasty1.jpeg",
+    title: "toasty 1",
+  },
+  {
+    id: 6,
+    image: "toasty2.jpeg",
+    title: "toasty 2",
+  },
+  {
+    id: 7,
+    image: "pepecat1.jpeg",
+    title: "pepecat 1",
+  },
+  {
+    id: 8,
+    image: "cave.jpeg",
+    title: "cave",
+  },
+  {
+    id: 9,
+    image: "noctra.jpeg",
+    title: "noctra",
+  },
+  {
+    id: 10,
+    image: "out-of-the-nest.jpeg",
+    title: "out of the nest",
+  },
+  {
+    id: 11,
+    image: "harold2.jpg",
+    title: "harold 2",
+  },
+  {
+    id: 12,
+    image: "chadonic.jpeg",
+    title: "chadonic",
+  },
+  {
+    id: 13,
+    image: "pepecat2.png",
+    title: "pepecat 2",
+  },
+  {
+    id: 14,
+    image: "bulls-club1.png",
+    title: "bulls club",
+  },
+  {
+    id: 15,
+    image: "harold.jpeg",
+    title: "harold 1",
+  },
+  {
+    id: 16,
+    image: "kulu1.jpeg",
+    title: "kulu 1",
+  },
+  {
+    id: 17,
+    image: "hege.jpeg",
+    title: "hege",
+  },
+  {
+    id: 18,
+    image: "kulu2.jpeg",
+    title: "kulu 2",
+  },
+  {
+    id: 19,
+    image: "titcoin.jpeg",
+    title: "titcoin",
+  },
+  {
+    id: 20,
+    image: "stash.jpeg",
+    title: "stash",
+  },
+  {
+    id: 21,
+    image: "grby.jpeg",
+    title: "grby",
+  },
+  {
+    id: 22,
+    image: "floki1.jpeg",
+    title: "floki 1",
+  },
+  {
+    id: 23,
+    image: "zeus.jpeg",
+    title: "zeus",
+  },
+  {
+    id: 24,
+    image: "springer.jpeg",
+    title: "springer 1",
+  },
+  {
+    id: 25,
+    image: "floki2.jpeg",
+    title: "floki 2",
+  },
+  {
+    id: 26,
+    image: "klaus.jpeg",
+    title: "klaus",
+  },
+  {
+    id: 27,
+    image: "springer2.jpeg",
+    title: "springer 2",
+  },
+  {
+    id: 28,
+    image: "zeus-pepecat&skop.jpeg",
+    title: "zeus, pepecat & skop",
+  },
+  {
+    id: 29,
+    image: "unfk.jpeg",
+    title: "unfk",
+  },
+  {
+    id: 30,
+    image: "fego.jpeg",
+    title: "fego",
+  },
+  {
+    id: 31,
+    image: "orc.jpeg",
+    title: "orc",
+  },
+  {
+    id: 32,
+    image: "iceland.jpeg",
+    title: "iceland",
+  },
+  {
+    id: 33,
+    image: "fartless.jpeg",
+    title: "fartless",
+  },
+  {
+    id: 34,
+    image: "fugz&gugo.jpeg",
+    title: "fugz & gugo",
+  },
+  {
+    id: 35,
+    image: "pexebel.jpeg",
+    title: "pexebel",
+  },
+  {
+    id: 36,
+    image: "bnkk1.jpeg",
+    title: "bnkk 1",
+  },
+  {
+    id: 37,
+    image: "bnkk2.jpeg",
+    title: "bnkk 2",
+  },
+  {
+    id: 38,
+    image: "sentient1.jpeg",
+    title: "sentient 1",
+  },
+  {
+    id: 39,
+    image: "sentient2.jpeg",
+    title: "sentient 2",
+  },
+  {
+    id: 40,
+    image: "sentient3.jpeg",
+    title: "sentient 3",
+  },
+  {
+    id: 41,
+    image: "sentient4.jpeg",
+    title: "sentient 4",
+  },
+  {
+    id: 42,
+    image: "sentient5.jpeg",
+    title: "sentient 5",
+  },
+  {
+    id: 43,
+    image: "sentient6.jpeg",
+    title: "sentient 6",
+  },
+  {
+    id: 44,
+    image: "hund.jpeg",
+    title: "hund",
+  },
+  {
+    id: 45,
+    image: "re.jpeg",
+    title: "re",
+  },
+  {
+    id: 46,
+    image: "bitfortune.jpeg",
+    title: "bitfortune",
+  },
+  {
+    id: 47,
+    image: "sentient7.jpeg",
+    title: "sentient 7",
+  },
+  {
+    id: 48,
+    image: "maestro.jpeg",
+    title: "maestro",
+  },
+  {
+    id: 49,
+    image: "dinosol1.jpeg",
+    title: "dinosol ",
+  },
+  {
+    id: 50,
+    image: "dinosol2.jpeg",
+    title: "dinosol 2",
+  },
+];
+
+export { pagesInfo };
